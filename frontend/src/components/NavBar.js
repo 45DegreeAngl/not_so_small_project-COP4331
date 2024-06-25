a16ea7530121ad7575666923588d1c335bf0a770
Shane Welz <ShaneWelz@gmail.com>
2024-06-24 20:42:15 -0400

    First commit to group 15 repo, mern stack demo is running for A and B

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,13 +1,19 @@
 import React, {useState} from 'react';
 
 const app_name = 'ganttify-5b581a9c8167'
+const baseStyle ={
+    backgroundColor:"#FDDC87", 
+    paddingLeft:"50px",
+    paddingTop:"10px",
+    paddingBottom:"10px"
+}
 function NavBar()
 {
 
 
     return(
-        <div id="navBarDiv">
-            <h2 id="appTitle" style="background-color:#FDDC87 padding-left:35">Ganttify</h2>
+        <div id="navBarDiv" style={baseStyle}>
+            <h2 id="appTitle">Ganttify</h2>
         </div>
     );
 }
