68c927febaa306ea42b8a1b200db7b3bb8c2a876
Shane Welz <ShaneWelz@gmail.com>
2024-07-01 19:56:40 -0400

    adding regex functionality to register

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -22,14 +22,46 @@ function Register(){
     var regPhone;
     var regEmail;
 
+    const validPhone = RegExp("/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/");
+    const validEmail = RegExp("");
+    const validPassLower= RegExp("/(?=.*[a-z])/");
+    const validPassUpper = RegExp("/(?=.*[A-Z])/");
+    const validPassSymbol = RegExp("/[A-Z]/i");
+    const validPassDigit = RegExp("/[0-9]/");
+
+    const msgTag = " *** ";
+
     const [message,setMessage] = useState('');
 
     const doRegister= async event =>{
         event.preventDefault();
-        if(regPassword.value !== regPasswordVerify.value){
-            setMessage("Passwords do not match");
+       
+        if(regPhone.length>0 && validPhone.test(regPhone) === false){
+            setMessage("*** Please enter a valid 10 digit phone number ***");
             return;
         }
+       
+        if (regPassword.value === null || regPassword.value.length < 8) {
+            setMessage("*** Your password must be at least 8 characters ***");
+            return;
+        }
+        if (validPassLower.test(regPassword.value) === false){
+            setMessage("*** Your password must contain at least one lowercase letter ***")
+            return;
+        }
+        if (validPassUpper.test(regPassword.value) === false) {
+            setMessage("*** Your password must contain at least one uppercase letter ***"); 
+            return;
+        }
+        if (validPassDigit.test(regPassword.value) === false) {
+            setMessage("*** Your password must contain at least one digit ***");
+            return;
+        } 
+        if(regPassword.value !== regPasswordVerify.value){
+            setMessage("*** Passwords do not match ***");
+            return;
+        }
+
 
         var obj = {email:regEmail.value,name:regName.value,phone:regPhone.value,password:regPassword.value,username:regUser.value};
         var js = JSON.stringify(obj);
@@ -42,7 +74,7 @@ function Register(){
             var res = JSON.parse(txt);
 
             if( res.error.length > 0 ){
-                    setMessage(res.error);
+                    setMessage(msgTag.concat(res.error,msgTag));
             }
             else{
                 setMessage("Account created")
@@ -53,27 +85,60 @@ function Register(){
             return;
         }
     }
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.getElementsByClassName('needs-validation');
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+            }, false);
+          });
+        }, false);
+      })();
 
     return(
-        <div className="registerComponent">
-            <h1 id="pageTitle" className='registerTitle'><b>Create an Account</b></h1>
-            <form onSubmit={doRegister}>
-                <div className='formTitle'><label for="accountName" id="nameTitle" ><b>Name*</b></label><br/></div>
-                <input required="" type="text" id="accountName" placeholder='Name' className='registerForm' ref={(c) => regName = c} /><br/>
-                <div className='formTitle'><label for="accountPhone" id="phoneTitle" ><b>Phone</b></label><br/></div>
-                <input type="tel" id="accountPhone" placeholder='Phone Number' className='registerForm' pattern="[0-9]{10}" title="Please enter a valid phone number" ref={(c) => regPhone = c}/><br/>
-                <div className='formTitle'><label for="accountEmail" id="emailTitle" ><b>Email*</b></label><br/></div>
-                <input required="" type="email" id="accountEmail" placeholder='Email' className='registerForm' ref={(c) => regEmail = c} /><br/>
-                <div className='formTitle'><label for="accountUsername" id="userTitle" ><b>Username*</b></label><br/></div>
-                <input required="" type="text" id="accountUsername" placeholder='Username' className='registerForm' ref={(c) => regUser = c} /><br/>
-                <div className='formTitle'><label for="accountPassword" id="passwordTitle" ><b>Password*</b></label><br/></div>
-                <input required type="password" id="accountPassword" placeholder='Password' className='registerForm' title="Password must be at least 8 characters and contain at least one capital letter, number and symbol" pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"  onchange="try{setCustomValidity('')}catch(e){}" oninput="setCustomValidity('')" ref={(c) => regPassword = c} /><br/>
-                <div className='formTitle'><label for="accountPasswordVerify" id="retypeTitle" ><b>Re-enter password*</b></label><br/></div>
-                <input required="" type="password" id="accountPasswordVerify" placeholder='Password' className='registerForm' title="Password must be at least 8 characters and contain at least one capital letter, number and symbol" pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$" onchange="try{setCustomValidity('')}catch(e){}" oninput="setCustomValidity('')" ref={(c) => regPasswordVerify = c} /><br/>
-                <h5 className="formDescription">* indicates a required field</h5>
-                <input type="submit" id="registerButton" className='registerButton' value='Create Account'/><br/>
-            </form>
-            <h5>{message}</h5>     
+        <div>
+            <div className='topDiv'>
+                <h1>Create an Account</h1>
+            </div>
+            <div class="mb-3 bottomDiv">
+                <form onSubmit={doRegister}>
+                    <div class="form-group">
+                        <label for="inputName"><h5><b>Name*</b></h5></label>
+                        <input type="text" class="form-control form-control-lg" id="inputName" placeholder='Firstname Lastname'  ref={(c) => regName = c} required/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputEmail"><h5><b>Email Address*</b></h5></label>
+                        <input type="email" class="form-control form-control-lg" id="inputEmail" placeholder='example@email.com' ref={(c) => regEmail = c} required/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPhone"><h5><b>Phone Number</b></h5></label>
+                        <input type="tel" class="form-control form-control-lg" id="inputPhone" placeholder='(###) ###-####' ref={(c) => regPhone = c}/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputUsermname"><h5><b>Username*</b></h5></label>
+                        <input type="text" class="form-control form-control-lg" id="inputUsername" placeholder='Username' ref={(c) => regUser = c} required/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword"><h5><b>Password*</b></h5></label>
+                        <input type="password" class="form-control form-control-lg" id="inputPassword" placeholder='Password1!' ref={(c) => regPassword = c}/>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword2"><h5><b>Re-enter Password*</b></h5></label>
+                        <input type="password" class="form-control form-control-lg" id="inputPassword2" placeholder='Password1!' ref={(c) => regPasswordVerify = c}/>
+                    </div>
+                    <div className='formDescription'><span>* indicates a required field</span></div>
+                    <div class="row justify-content-center buttonDiv"><button type="submit" class="btn submitButton">Create Account</button></div>
+                </form>
+                <div className='formMessage'><span>{message}</span></div>
+            </div>
         </div>
     );
     
