ccbf54f1e8a0f67867fb48f5deb3121d7d294ae3
lostkeyz <keylizrodz@gmail.com>
2024-07-08 00:02:22 -0400

    Update ResetPassword.js
    Updated functionalities of the page.

--- frontend/src/components/ResetPassword.js
+++ frontend/src/components/ResetPassword.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './ResetPassword.css';
 const app_name = 'ganttify-5b581a9c8167';
 
@@ -12,48 +12,52 @@ function buildPath(route) {
 }
 
 function ResetPassword() {
-  const { id, token } = useParams();
+
+  const params = useParams();
+  let { id } = params;
+
   const validPassLower = RegExp("[a-z]+");
   const validPassUpper = RegExp("[A-Z]+");
-  const validPassSymbol = RegExp("[^a-zA-Z0-9\s]+");
+  const validPassSymbol = RegExp("[^a-zA-Z0-9]+");
   const validPassDigit = RegExp("[0-9]+");
 
-  const msgTag = " *** ";
-
-  const [message, setMessage] = useState('');
   const [password, setPassword] = useState('');
   const [verifiedPassword, setVerifiedPassword] = useState('');
+  const [disableButton, setDisableButton] = useState(false);
+  const navigate = useNavigate();
 
   const doResetPassword = async event => {
     event.preventDefault();
 
     if (password === null || password.length < 8) {
-      setMessage("*** Your password must be at least 8 characters ***");
+      window.alert('*** Your password must be at least 8 characters ***');
       return;
     }
     if (!validPassLower.test(password)) {
-      setMessage("*** Your password must contain at least one lowercase letter ***");
+      window.alert('*** Your password must contain at least one lowercase letter ***');
       return;
     }
     if (!validPassUpper.test(password)) {
-      setMessage("*** Your password must contain at least one uppercase letter ***");
+      window.alert('*** Your password must contain at least one uppercase letter ***');
       return;
     }
     if (!validPassDigit.test(password)) {
-      setMessage("*** Your password must contain at least one digit ***");
+      window.alert('*** Your password must contain at least one digit ***');
       return;
     }
     if (!validPassSymbol.test(password)) {
-      setMessage("*** Your password must contain at least special symbol ***");
+      window.alert('*** Your password must contain at least special symbol ***');
       return;
     }
 
     if (password !== verifiedPassword) {
-      setMessage("*** Passwords do not match ***");
+      window.alert('*** Passwords do not match ***');
       return;
     }
 
-    var obj = { id, token, password };
+    setDisableButton(true);
+
+    var obj = { id: id, password: password };
     var js = JSON.stringify(obj);
     try {
       const response = await fetch(buildPath('api/reset-password'), {
@@ -66,41 +70,45 @@ function ResetPassword() {
       var res = JSON.parse(txt);
 
       if (res.error) {
-        setMessage(res.error);
+        window.alert("There was an issue with your password reset.");
       } else {
-        setMessage("Password has been reset successfully");
+        window.alert("Password has been reset successfully!");
+        navigate("/login");
       }
+
     } catch (e) {
       alert(e.toString());
       return;
+    } finally {
+      setDisableButton(false);
     }
   };
 
   return (
     <div className="reset-password-container">
 
-        <form className="reset-password-form" onSubmit={doResetPassword}>
+      <form className="reset-password-form" onSubmit={doResetPassword}>
 
-            <h1 className="reset-password-title">Reset Password</h1>
+        <h1 className="reset-password-title">Reset Password</h1>
 
 
-            <div className="form-floating pb-2">
-                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
-                <label htmlFor="floatingPassword">New password</label>
-            </div>
+        <div className="form-floating pb-2">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+            <label htmlFor="floatingPassword">New password</label>
+        </div>
 
-            <div className="form-floating pb-2">
-                <input type="password" className="form-control" id="floatingPassword2" placeholder="Password" value={verifiedPassword} onChange={e => setVerifiedPassword(e.target.value)} required />
-                <label htmlFor="floatingPassword2">Re-enter new password</label>
-            </div>
+        <div className="form-floating pb-2">
+            <input type="password" className="form-control" id="floatingPassword2" placeholder="Password" value={verifiedPassword} onChange={e => setVerifiedPassword(e.target.value)} required />
+            <label htmlFor="floatingPassword2">Re-enter new password</label>
+        </div>
 
-            <div className="button-container d-grid gap-2">
-                <button className="btn btn-primary" type="submit">Reset Password</button>
-                <a href="/login" className="btn-2 btn-link mt-2">Back to Login</a>
-            </div>
-
-        </form>
-
+        <div className="button-container d-grid gap-2">
+            <button className="btn btn-primary" type="submit" disabled = {disableButton}>
+            {disableButton ? 'Resetting...' : 'Reset Password'}
+            </button>
+            <a href="/login" className="btn-2 btn-link mt-2">Back to Login</a>
+        </div>
+      </form>
     </div>
   );
 }
