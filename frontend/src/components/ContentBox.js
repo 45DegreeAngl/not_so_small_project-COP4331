599b3dd39598e6f346c4ad502b0ac87dfc2ebfce
lostkeyz <keylizrodz@gmail.com>
2024-07-26 11:10:26 -0400

    Update ContentBox.js

--- frontend/src/components/ContentBox.js
+++ frontend/src/components/ContentBox.js
@@ -1,3 +1,4 @@
+
 import Start from '../Images/People_Working_Temp.jpg';
 import Plan from '../Images/People_Planning_Temp.jpg';
 import Complete from '../Images/Hand_Shaking_Temp.jpg';
@@ -10,74 +11,77 @@ function ContentBox(props){
 
     if(props.layout == 0){
         return(
-            <div>
+            <div> 
                 <div>
-                    <p class = "info">
-                        Ganttify is a project management tool that helps you plan, organize, and track your projects in a visually appealing way.
-                    </p>
-
+                    <div className = "infoBox">
+                        <p className = "info">
+                            Ganttify is a project management tool that helps you plan, organize, and track your projects in a visually appealing way.
+                        </p>
+                    </div>
+        
+                    <div className = "Start">
+                        <h1 className = "animated-header-1">
+                            Start
+                        </h1>
+        
+                        <p className = "textbox">
+                        Take advantage of Ganttify's simple onboarding process to plan your day! Everyone gets Ganttify and all of its features for free. Simply sign up with a valid email address.
+                        </p>
+        
+                        <a href = "/">
+                                <img src={Start} alt="" className="start_image" />
+                        </a>
+        
+                        <div className = "Box"></div>
+                    </div>
+        
+                    <div className = "Plan">
+                        <a href = "/">
+                                <img src={Plan} alt="" className="plan_image" />
+                        </a>
+        
+                        <p className = "textboxPlan">                   
+                                At Ganttify, there's only one plan: free, simple and accessible to everyone! Divide and conquer by assigning team members to tasks within a chart. 
+                                Use a color picker and pattern overlays for maximum visual constrast for task colors. 
+                                If you do not want other users to see or team members to join your chart, simply make it private.
+                        </p>
+        
+                        <h1 className = "animated-header-1">
+                            Plan
+                        </h1>
+        
+                        <div className = "Box"></div>
+                    </div>
+        
+        
+                    <div className = "Complete">
+                        <h1 className = "animated-header-2">
+                            Complete
+                        </h1>
+        
+                        <p className = "textbox">
+                        Simply toggle a task to indicate if it's done. You can mark tasks as complete on desktop or in our companion app.
+                        </p>
+        
+                        <a href = "/">
+                                <img src={Complete} alt="" className="complete_image" />
+                        </a>
+        
+                        <div className = "Box"></div>
+                    </div>
+        
+                    
                 </div>
-
-                <div className = "Start">
-                    <h1 class = "animated-header-1">
-                        Start
-                    </h1>
-
-                    <p class = "textbox">
-                    Take advantage of Ganttify's simple onboarding process to plan your day! Everyone gets Ganttify and all of its features for free. Simply sign up with a valid email address.
-                    </p>
-
-                    <a href = "/">
-                            <img src={Start} alt="" className="start_image" />
-                    </a>
-
-                    <div class = "Box"></div>
+                <div>
+                    <div className="EndTagWrapper">
+                        <div className = "endTag">
+                            <div className = "align">
+                                <p>Learn More</p>
+                                <li><a href = "/about-us"><button className="button">About Us</button></a></li> 
+                        </div>    
+                    </div>            
                 </div>
-
-                <div className = "Plan">
-                    <a href = "/">
-                            <img src={Plan} alt="" className="plan_image" />
-                    </a>
-
-                    <p class = "textbox">
-                    At Ganttify, there's only one plan: free, simple and accessible to everyone! Divide and conquer by assigning team members to tasks within a chart. 
-                    Use a color picker and pattern overlays for maximum visual constrast for task colors. 
-                    If you do not want other users to see or team members to join your chart, simply make it private.
-                    </p>
-
-                    <h1 class = "animated-header-2">
-                        Plan
-                    </h1>
-
-                    <div class = "Box"></div>
                 </div>
-
-
-                <div className = "Complete">
-                    <h1 class = "animated-header-1">
-                        Complete
-                    </h1>
-
-                    <p class = "textbox">
-                    Simply toggle a task to indicate if it's done. You can mark tasks as complete on desktop or in our companion app.
-                    </p>
-
-                    <a href = "/">
-                            <img src={Complete} alt="" className="complete_image" />
-                    </a>
-
-                    <div class = "Box"></div>
-                </div>
-
-                <div class = "endTag">
-                        <div class = "align">
-                            <p>Learn More   <li><a href = "/about-us"><button class="button">About Us</button></a></li> </p>
-                        </div>                
-                </div>
-
-
-
-
             </div>
   
         );
