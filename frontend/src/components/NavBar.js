32c105d3834ed582b5a17c5415fe31dbdf13f5b6
lostkeyz <keylizrodz@gmail.com>
2024-07-28 02:01:27 -0400

    Update NavBar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -49,13 +49,13 @@ async function createTask(newTask) {
   }
 }
 
-
 function NavBar(props) {
   const [showModal, setShowModal] = useState(false);
   const [inviteModal, setInviteModal] = useState(false);
   const [teamMembers, setTeamMembers] = useState([]);
   const [inviteEmail, setInviteEmail] = useState("");
   const [inviteMessage, setInviteMessage] = useState("");
+  
   const [taskData, setTaskData] = useState({
     taskTitle: "",
     description: "",
@@ -65,6 +65,8 @@ function NavBar(props) {
     color: "#DC6B2C",
     pattern: "default-pattern"
   });
+
+  const [isEditor, setisEditor] = useState(false); 
 
   var _ud = localStorage.getItem('user_data');
   var ud = JSON.parse(_ud);
@@ -96,16 +98,22 @@ function NavBar(props) {
         const { usersInfo } = await userResponse.json();
         const validUsers = Array.isArray(usersInfo) ? filterValidUsers(usersInfo) : [];
         setTeamMembers(validUsers);
+
+        
+        if (userId === team.founderId || team.editors.includes(userId)) {
+          setisEditor(true);
+        }
       }
     } catch (error) {
       console.error('Error fetching team members:', error);
     }
   };
 
-  
   const filterValidUsers = (users) => {
     return users.filter(user => user !== null);
   };
+
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -115,10 +123,11 @@ function NavBar(props) {
     }));
   };
 
-  
   const handleInviteEmailChange = (e) => {
     setInviteEmail(e.target.value);
   };
+
+
 
   const handleInviteSubmit = async () => {
     if (!inviteEmail.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
@@ -132,6 +141,8 @@ function NavBar(props) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email: inviteEmail, projectId }),
       });
+
+
 
       const result = await response.json();
 
@@ -149,9 +160,8 @@ function NavBar(props) {
     }
   };
 
-  
-
   const handleAddTask = async (e) => {
+
     e.preventDefault();
     try {
       const newTask = {
@@ -201,13 +211,11 @@ function NavBar(props) {
           </a>
           <h1> Ganttify </h1>
           <ul>
-      
             <li><Link to="/"><button id="button"> Home</button></Link></li>
             <li><Link to="/about-us"><button id="button">About Us</button></Link></li>
             <li><Link to="/register"><button id="button">Create Account</button></Link></li>
             <li><Link to="/login"><button id="button">Login</button></Link></li>
           </ul>
-      
         </div>
       </div>
     );
@@ -227,31 +235,36 @@ function NavBar(props) {
     );
   } else if (props.layout == 3) {
     return (
-      
+
+
       <div className="layout-3">
         <div id="navBarDiv" style={dashboardNav} role="navigation">
           <div className="navbarDash">
-      
             <a href="/" aria-label="Go back to home page">
               <img src={Logo} alt="GanttifyHomePage" className="logo" />
             </a>
-      
             <ProjectTitle projectId={projectId} />
             <ul>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Team
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  {teamMembers.map(member => (
-                    <a key={member._id} className="dropdown-item">{member.name}</a>
-                  ))}
-                  <div className="dropdown-divider"></div>
-                  <a className="dropdown-header" onClick={openInviteModal}>Invite Team Members</a>
-                </div>
-              </li>
+
+              {isEditor && ( 
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Team
+                  </a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    {teamMembers.map(member => (
+                      <a key={member._id} className="dropdown-item">{member.name}</a>
+                    ))}
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-header" onClick={openInviteModal}>Invite Team Members</a>
+                  </div>
+                </li>
+              )}
+
               <li><Link to="/dashboard"><button id="button">Dashboard</button></Link></li>
               <li><Link to="/"><button id="button">Sign Out</button></Link></li>
+
+
             </ul>
           </div>
         </div>
@@ -260,68 +273,74 @@ function NavBar(props) {
         <div className="modal fade modal-custom" id="addTaskModal" tabIndex="-1" aria-labelledby="addTaskModalLabel" aria-hidden="true">
           <div className="modal-dialog">
             <div className="modal-content">
+
               <div className="modal-header">
                 <h1 className="modal-title fs-5" id="addTaskModalLabel">Add a Task</h1>
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={closeModal}></button>
               </div>
+
+
               <div className="modal-body">
+
                 <form onSubmit={handleAddTask}>
-                    
                   <div className="mb-3">
                     <label htmlFor="taskTitle" className="form-label">Task Title</label>
                     <input type="text" className="form-control" id="taskTitle" name="taskTitle" value={taskData.taskTitle} onChange={handleInputChange} required />
                   </div>
-                    
+
+
                   <div className="mb-3">
                     <label htmlFor="description" className="form-label">Description</label>
                     <textarea className="form-control" id="description" name="description" value={taskData.description} onChange={handleInputChange}></textarea>
                   </div>
-                    
+
+
                   <div className="mb-3">
                     <label htmlFor="startDateTime" className="form-label">Start Date</label>
                     <input type="date" className="form-control" id="startDateTime" name="startDateTime" value={taskData.startDateTime} onChange={handleInputChange} required />
                   </div>
-                    
+
+
                   <div className="mb-3">
                     <label htmlFor="dueDateTime" className="form-label">Due Date</label>
                     <input type="date" className="form-control" id="dueDateTime" name="dueDateTime" value={taskData.dueDateTime} onChange={handleInputChange} required />
                   </div>
 
-                    
+
                   <div className="mb-3">
                     <label htmlFor="assignedTasksUsers" className="form-label">Assigned Users (comma separated IDs)</label>
                     <input type="text" className="form-control" id="assignedTasksUsers" name="assignedTasksUsers" value={taskData.assignedTasksUsers.join(',')} onChange={(e) =>
-                      setTaskData((prevData) => ({
-                        ...prevData,
-                        assignedTasksUsers: e.target.value.split(','),
-                      }))
-                    } />
-
-                      
+                      setTaskData((prevData) => ({...prevData, assignedTasksUsers: e.target.value.split(','), }))} />
                   </div>
+
                   <div className="mb-3">
                     <label htmlFor="color" className="form-label">Color</label>
                     <input type="color" className="form-control" id="color" name="color" value={taskData.color} onChange={handleInputChange} />
                   </div>
 
-                      
+
                   <div className="mb-3">
                     <label htmlFor="pattern" className="form-label">Pattern</label>
                     <input type="text" className="form-control" id="pattern" name="pattern" value={taskData.pattern} onChange={handleInputChange} />
                   </div>
-                      
+
+
                   <button type="submit" className="btn btn-primary">Add Task</button>
                 </form>
               </div>
-                      
+
+
               <div className="modal-footer">
                 <h5 className="message">{props.message}</h5>
               </div>
+
             </div>
           </div>
         </div>
 
+
         <div className={`modal ${inviteModal ? 'show' : ''}`} tabIndex="-1" role="dialog" style={{ display: inviteModal ? 'block' : 'none' }}>
+                        
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header">
@@ -330,17 +349,20 @@ function NavBar(props) {
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-                      
+
+
               <div className="modal-body">
                 <p>Enter the email address of the person you want to invite to the team.</p>
                 <input type="email" className="form-control" value={inviteEmail} onChange={handleInviteEmailChange} placeholder="Email address" required />
                 <div className="invite-message">{inviteMessage}</div>
-                      
               </div>
+
+
               <div className="modal-footer">
                 <button type="button" className="btn btn-primary" onClick={handleInviteSubmit}>Send Invite</button>
               </div>
-                      
+
+              
             </div>
           </div>
         </div>
